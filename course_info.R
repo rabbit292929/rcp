1e02b3a011af2ebcded9aaf869974dc49b4dff68
Michael Lydeamore <michael.lydeamore@monash.edu>
2025-02-26 19:56:46 +1100

    Move assignments to a section block

--- course_info.R
+++ course_info.R
@@ -87,9 +87,9 @@ assignments <- read_csv(here::here("assignments.csv")) |>
         File = paste0("assignments/", File)
     )
 
-schedule <- schedule |>
-    full_join(assignments, by = "Date") |>
-    mutate(Week = if_else(is.na(Week) & Date > "2024-05-20", 13, Week))
+#schedule <- schedule |>
+#    full_join(assignments, by = "Date") |>
+#    mutate(Week = if_else(is.na(Week) & Date > "2024-05-20", 13, Week))
 
 show_assignments <- function(week) {
     ass <- schedule |>
